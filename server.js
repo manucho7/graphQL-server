ad5cdac0d155869c1df1698760be994ec7e268a5
manucho <manuel.blanco77@gmail.com>
2021-02-23 18:40:09 -0300

    Unifying apolloServer with express server in server.js

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
 //imports
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const http = require('http');
 require('dotenv').config();
 
 //initializing express server | req-res handler
 const app = express();
 
-//1st argument is endpoint, 2nd argument callback fn() 
-//REST API endpoint
+//graphql query
+const typeDefs = `
+    type Query {
+        totalPosts: Int!
+    }
+`
+
+//resolvers fn that will resolve the query
+
+const resolvers = {
+    Query: {
+        totalPosts: () => 42
+    }
+};
+
+//GraphQL server
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+// Apply middleware method connects Apollo server to a specific
+//http framework ei: express
+
+apolloServer.applyMiddleware({app});
+
+//server
+const httpServer= http.createServer(app);
+
+//REST API endpoint / graphql
 app.get('/rest', (req, res) => {
     res.json({
         data: 'you hit rest endpoint'
@@ -15,4 +45,5 @@ app.get('/rest', (req, res) => {
 
 app.listen(process.env.PORT, () => {
     console.log(`server initialized at http://localhost:${process.env.PORT}`);
+    console.log(`graphql server initialized at http://localhost:${process.env.PORT}${apolloServer.graphqlPath}`);
 });
